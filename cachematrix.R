6ecd172d25a7d66a4c0b0e4b2d4382e1634a30d0
AlgoTrade <gianluca.ciocci@google.com>
2014-10-25 13:31:11 +0100

    Revert "Bug fixing"
    This reverts commit cf4576235d73a59c18f01c0cadcf2de32799d4bf.

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,14 @@
 ##  that can cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-    mInv <- NULL
+    m <- NULL
     ##  set the value of the matrix (set function)
     set <- function(y){
         x <<- y
         mInv <<- NULL      
     }
     ## get the value of the matrix (get function)
-    get <- function() x
+    get function() x
     
     ## set the value of the inverse (setMInv(inv) function)
     setMInv <- function(inv) mInv <<- inv
@@ -18,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
     ## get the value of the inverse (getMInv function)
     getMInv <- function() mInv
     
-    list(set = set, get = get, setMInv = setMInv, getMInv = getMInv )
+    list(set = set, get = get, setMInv = setMInv, getMinv = getMInv )
     
 }
 
@@ -41,23 +41,3 @@ cacheSolve <- function(x, ...) {
     x$setMInv(mInv)
     mInv
 }
-
-## Test run
-##> source('F:/RProgramming/ProgrammingAssignment2/cachematrix.R')
-##> x <- matrix(c(2,1,4,3), nrow=2,ncol=2)
-##> m <- makeCacheMatrix(x)
-##> m$get()
-##[,1] [,2]
-##[1,]    2    4
-##[2,]    1    3
-##> cacheSolve(m)
-##[,1] [,2]
-##[1,]  1.5   -2
-##[2,] -0.5    1
-##> cacheSolve(m)
-##getting cached data...
-##[,1] [,2]
-##[1,]  1.5   -2
-##[2,] -0.5    1
-
-
