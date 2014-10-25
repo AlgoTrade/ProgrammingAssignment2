b39943bba83c416d23c81e4a1494141decb3686a
AlgoTrade <gianluca.ciocci@google.com>
2014-10-25 12:27:42 +0100

    makeChaceMatrix coded

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##  makeCacheMatrix function creates/store a matrix object 
+##  that can cache its inverse.
+#1. set the value of the matrix (set function)
+#2. get the value of the matrix (get function)
+#3. set the value of the inverse (setMInv(inv) function)
+#4. get the value of the inverse (getMInv function)
 
 makeCacheMatrix <- function(x = matrix()) {
-    test <- 1:10
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        mInv <<- NULL      
+    }
+    get function() x
+    setMInv <- function(inv) mInv <<- inv
+    getMInv <- function() mInv
+    list(set = set, get = get, setMInv = setMInv, getMinv = getMInv )
+    
 }
 
 
